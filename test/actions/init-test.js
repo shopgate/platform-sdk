a2c3aaf1d4e3076ee060e209eae79de07aab45f9
Metin Kul <metin.kul@shopgate.com>
2017-10-24 18:17:28 +0200

    added unit-tests for everything but inquirer prompt

--- test/actions/init-test.js
+++ test/actions/init-test.js
@@ -1,45 +1,92 @@
-const action = require('../../lib/actions/init')
+const init = require('../../lib/actions/init')
 const assert = require('assert')
+const UserSettings = require('../../lib/user/UserSettings')
+const AppSettings = require('../../lib/app/AppSettings')
+const path = require('path')
+const userSettingsFolder = path.join('test', 'usersettings')
+const appPath = path.join('test', 'appsettings')
+const rimraf = require('rimraf')
+const mkdirp = require('mkdirp')
 
-describe('init', () => {
+describe('actions', () => {
   describe('init', () => {
-    it('should throw (not implemented yet)', () => {
-      assert.throws(action)
-    })
-
-    it('should have a cmd function', (done) => {
-      let doneCount = 0
-      function isDone () {
-        if (doneCount++ === 3) done()
-      }
-
-      assert.equal(typeof action.cmd, 'function')
-
+    it('should set itself up in commander', () => {
       const commander = {
-        command: (c) => {
+        command: c => {
           assert.equal(c, 'init')
-          isDone()
           return commander
         },
-        description: (d) => {
-          assert.equal(d, 'init the sdk')
-          isDone()
+        description: d => {
+          assert.ok(d)
           return commander
         },
-        action: (a) => {
-          assert.equal(a, action)
-          isDone()
+        option: (o, d) => {
+          assert.equal(o, '--appId <appId>')
+          assert.ok(d)
           return commander
         },
-        option: (opt, desc) => {
-          assert.equal(opt, '--appId <appId>')
-          assert.equal(desc, 'set the Application ID you want to initialize')
-          isDone()
+        action: a => {
+          assert.equal(a, init)
           return commander
         }
       }
+      init.cmd(commander)
+    })
 
-      action.cmd(commander)
+    beforeEach(() => {
+      process.env.USER_PATH = userSettingsFolder
+    })
+
+    afterEach((done) => {
+      UserSettings.destroyInstance()
+      delete process.env.USER_PATH
+      rimraf(userSettingsFolder, done)
+    })
+
+    it('should throw if user not logged in', (done) => {
+      UserSettings.getInstance().getSession().token = null
+      try {
+        init()
+      } catch (err) {
+        assert.equal(err.message, 'not logged in')
+        done()
+      }
+    })
+
+    it('should throw if application already initialized', (done) => {
+      UserSettings.getInstance().getSession().token = {}
+      const appId = 'foobarTest'
+
+      process.env.APP_PATH = appPath
+      const appSettings = new AppSettings()
+      mkdirp.sync(path.join(appPath, AppSettings.SETTINGS_FOLDER))
+      appSettings.setId(appId).save().init()
+      AppSettings.setInstance(appSettings)
+
+      let wasCatched = false
+      try {
+        init()
+      } catch (err) {
+        wasCatched = true
+        assert.equal(err.message, `The current folder is already initialized for application ${appId}`)
+      }
+
+      assert.ok(wasCatched)
+      delete process.env.APP_PATH
+      AppSettings.destroyInstance()
+      rimraf(appPath, done)
+    })
+
+    it('should create folders, settings file and save appId', (done) => {
+      AppSettings.destroyInstance()
+      process.env.APP_PATH = appPath
+      UserSettings.getInstance().getSession().token = {}
+
+      init({appId: 'test'}, () => {
+        delete process.env.APP_PATH
+        AppSettings.destroyInstance()
+        rimraf(appPath, done)
+      })
     })
   })
 })
