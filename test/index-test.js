169a964f98dd1e2f9ed45c2c780ff37368c47a9d
Florian Müller <florian.mueller@shopgate.com>
2017-10-26 11:45:15 +0200

    CON-2551 Added restify http server

--- test/index-test.js
+++ test/index-test.js
@@ -5,7 +5,7 @@ const assert = require('assert')
 
 describe('main', () => {
   it('should export each action', () => {
-    const actionFiles = glob.sync('../lib/actions/*.js', {cwd: __dirname, strict: true})
+    const actionFiles = glob.sync('../lib/actions/**/*.js', {cwd: __dirname, strict: true})
     const actions = {}
     actionFiles.forEach((actionFile) => {
       const actionName = path.basename(actionFile).split('.')[0]
