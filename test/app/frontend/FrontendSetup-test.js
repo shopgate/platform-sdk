482f074863d2722ba9325e033e553333f806dcb8
Florian Müller <florian.mueller@shopgate.com>
2017-11-03 13:53:29 +0100

    CON-2542 Finished tests for Frontend Setup

--- test/app/frontend/FrontendSetup-test.js
+++ test/app/frontend/FrontendSetup-test.js
@@ -10,15 +10,34 @@ const rimraf = require('rimraf')
 const mkdirp = require('mkdirp')
 const sinon = require('sinon')
 const assert = require('assert')
+const proxyquire = require('proxyquire')
 const UserSettings = require('../../../lib/user/UserSettings')
 const AppSettings = require('../../../lib/app/AppSettings')
-const FrontendSetup = require('../../../lib/app/frontend/FrontendSetup')
+
+let requestSpyFail = false
+let requestStatusCode = 201
+const requestSpyError = 'Request failed!'
+const requestSpyBody = 'Some Body content'
+const request = sinon.spy(function (url, callback) {
+  if (requestSpyFail) {
+    callback(new Error(requestSpyError))
+  }
+
+  callback(null, { statusCode: requestStatusCode }, requestSpyBody)
+})
+const loggerSpy = sinon.spy()
+
+const FrontendSetup = proxyquire('../../../lib/app/frontend/FrontendSetup', {
+  request,
+  '../../logger': {
+    plain: loggerSpy
+  }
+})
 
 const userSettingsPath = join('test', 'usersettings')
 const appSettingsPath = join('test', 'appsettings')
 const appId = 'foobarTest'
 
-let frontendSetup
 const defaultConfig = {
   ip: '0.0.0.0',
   port: 8080,
@@ -29,6 +48,13 @@ const defaultConfig = {
   accessToken: 'abcabcabcabc'
 }
 const runError = 'Had an error'
+
+const setPromptBody = (confirmed = true) => (questions = []) =>
+  questions
+    ? Promise.resolve({ ...defaultConfig, confirmed })
+    : Promise.reject(runError)
+
+let frontendSetup
 
 describe('FrontendSetup', () => {
   beforeEach(() => {
@@ -46,11 +72,7 @@ describe('FrontendSetup', () => {
     frontendSetup.defaultConfig = defaultConfig
     frontendSetup.logHelper.logSetupLogo = () => {}
 
-    frontendSetup.save = () => {}
-    frontendSetup.prompt = (questions = []) =>
-      questions
-        ? Promise.resolve({ ...defaultConfig, confirmed: true })
-        : Promise.reject(runError)
+    frontendSetup.prompt = setPromptBody()
   })
 
   afterEach((done) => {
@@ -64,69 +86,238 @@ describe('FrontendSetup', () => {
   describe('FrontendSetup.run()', () => {
     beforeEach(() => {
       frontendSetup.registerSettings = () => {}
+      frontendSetup.save = () => {}
     })
 
-    it('should throw an error if something goes wrong', async () => {
-      try {
-        const spy = sinon.spy(frontendSetup, 'run')
-        frontendSetup.getQuestions = () => false
-
-        await frontendSetup.run()
-        assert.ok(spy.calledOnce)
-        assert.ok(spy.threw())
-        spy.restore()
-      } catch (error) {}
+    it('should throw an error if something goes wrong', (done) => {
+      const spy = sinon.spy(frontendSetup, 'run')
+      frontendSetup.getQuestions = () => false
+      frontendSetup.run()
+        .then(() => {
+          spy.reset()
+          done('Did not throw!')
+        })
+        .catch((error) => {
+          assert.ok(typeof error === 'string')
+          assert.ok(spy.calledOnce)
+          spy.reset()
+          done()
+        })
     })
 
-    it('should build the questions', async () => {
+    it('should build the questions', (done) => {
       const spy = sinon.spy(frontendSetup, 'getQuestions')
-
-      await frontendSetup.run()
-      assert.ok(spy.calledOnce)
-      assert.ok(Array.isArray(spy.returnValues[0]))
-      spy.restore()
+      frontendSetup.run()
+        .then(() => {
+          assert.ok(spy.calledOnce)
+          assert.ok(Array.isArray(spy.returnValues[0]))
+          spy.reset()
+          done()
+        })
+        .catch(error => done(error))
     })
 
-    it('should inquire the answers', async () => {
+    it('should inquire the answers', (done) => {
       const spy = sinon.spy(frontendSetup, 'prompt')
-
-      await frontendSetup.run()
-      assert.ok(spy.calledOnce)
-      spy.restore()
+      frontendSetup.run()
+        .then(() => {
+          assert.ok(spy.calledOnce)
+          spy.reset()
+          done()
+        })
+        .catch(error => done(error))
     })
 
-    it('should register the settings', async () => {
+    it('should register the settings', (done) => {
       const spy = sinon.spy(frontendSetup, 'registerSettings')
-
-      await frontendSetup.run()
-      assert.ok(spy.calledOnce)
-      spy.restore()
+      frontendSetup.run()
+        .then(() => {
+          assert.ok(spy.calledOnce)
+          spy.reset()
+          done()
+        })
+        .catch(error => done(error))
     })
 
-    it('should save the settings', async () => {
+    it('should save the settings', (done) => {
       const spy = sinon.spy(frontendSetup, 'save')
-
-      await frontendSetup.run()
-      assert.ok(spy.calledOnce)
-      spy.restore()
+      frontendSetup.run()
+        .then(() => {
+          assert.ok(spy.calledOnce)
+          spy.reset()
+          done()
+        })
+        .catch(error => done(error))
     })
   })
 
   describe('FrontendSetup.registerSettings()', () => {
-    it('should throw an error if settings not confirmed', (done) => {
-      frontendSetup.prompt = (questions = []) =>
-        questions
-          ? Promise.resolve({ ...defaultConfig, confirmed: false })
-          : Promise.reject(runError)
+    beforeEach(() => {
+      frontendSetup.save = () => {}
+    })
 
+    it('should call the createRequestUrl()', (done) => {
+      const requestUrlSpy = sinon.spy(frontendSetup, 'createRequestUrl')
+      frontendSetup.prompt = setPromptBody()
+      frontendSetup.run()
+        .then(() => {
+          sinon.assert.calledOnce(requestUrlSpy)
+          request.reset()
+          requestUrlSpy.reset()
+          done()
+        })
+        .catch((error) => {
+          done(error)
+        })
+    })
+
+    it('should process a request to trigger the jenkins job', (done) => {
+      frontendSetup.prompt = setPromptBody()
+      frontendSetup.run()
+        .then(() => {
+          sinon.assert.calledOnce(request)
+          request.reset()
+          done()
+        })
+        .catch((error) => {
+          done(error)
+        })
+    })
+
+    it('should throw an error if settings not confirmed', (done) => {
+      frontendSetup.prompt = setPromptBody(false)
       frontendSetup.run()
         .then(() => {
           done('Did not throw!')
         })
         .catch((error) => {
+          sinon.assert.notCalled(request)
           assert.equal(error.message, 'Sorry, you canceled the setup! Please try again.')
+          request.reset()
           done()
         })
+    })
+  })
+
+  describe('FrontendSetup.handleRequestResponse()', () => {
+    beforeEach(() => {
+      frontendSetup.save = () => {}
+    })
+
+    it('should not throw an error if all is good', (done) => {
+      const handlerSpy = sinon.spy(frontendSetup, 'handleRequestResponse')
+
+      frontendSetup.prompt = setPromptBody()
+      frontendSetup.run()
+        .then(() => {
+          assert.ok(handlerSpy.calledOnce)
+          handlerSpy.reset()
+          request.reset()
+          done()
+        })
+        .catch((error) => {
+          handlerSpy.reset()
+          request.reset()
+          done(error)
+        })
+    })
+
+    it('should throw if an error happened', (done) => {
+      const handlerSpy = sinon.spy(frontendSetup, 'handleRequestResponse')
+      requestSpyFail = true
+
+      frontendSetup.prompt = setPromptBody()
+      frontendSetup.run()
+        .then(() => {
+          handlerSpy.reset()
+          request.reset()
+          requestSpyFail = false
+          done('Did not throw!')
+        })
+        .catch((error) => {
+          assert.equal(error.message, requestSpyError)
+          requestSpyFail = false
+          handlerSpy.reset()
+          request.reset()
+          done()
+        })
+    })
+
+    it('should throw if the status code is wrong', (done) => {
+      const handlerSpy = sinon.spy(frontendSetup, 'handleRequestResponse')
+      requestStatusCode = 300
+
+      frontendSetup.prompt = setPromptBody()
+      frontendSetup.run()
+        .then(() => {
+          handlerSpy.reset()
+          request.reset()
+          requestStatusCode = 201
+          done('Did not throw!')
+        })
+        .catch((error) => {
+          assert.equal(error.message, `Wrong statusCode ${requestStatusCode}. Message: ${requestSpyBody}`)
+          handlerSpy.reset()
+          request.reset()
+          requestStatusCode = 201
+          done()
+        })
+    })
+  })
+
+  describe('FrontendSetup.createRequestUrl()', () => {
+    beforeEach(() => {
+      frontendSetup.save = () => {}
+    })
+
+    it('should create the request url', () => {
+      const ip = '0.0.0.0'
+      const port = 9666
+      const accessToken = 'abcabcabcabc'
+      const appId = 'sometest'
+      const url = frontendSetup.createRequestUrl(ip, port, accessToken, appId)
+
+      assert.equal(url, frontendSetup.createRequestUrl(ip, port, accessToken, appId))
+      assert.ok(typeof url === 'string')
+    })
+  })
+
+  describe('FrontendSetup.save()', () => {
+    let saveSpy
+
+    beforeEach(() => {
+      saveSpy = sinon.spy()
+      loggerSpy.reset()
+    })
+
+    afterEach(() => {
+      saveSpy.reset()
+      loggerSpy.reset()
+    })
+
+    it('should save the settings', () => {
+      frontendSetup.save(defaultConfig)
+      const saveSettings = frontendSetup.settings.getFrontendSettings().settings
+      assert.equal(JSON.stringify(saveSettings), JSON.stringify(defaultConfig))
+    })
+
+    it('should show two console logs on success', () => {
+      frontendSetup.save(defaultConfig)
+      sinon.assert.calledTwice(loggerSpy)
+    })
+
+    it('should throw if something went wrong', (done) => {
+      frontendSetup.settings.getFrontendSettings = () => {
+        throw new Error('error')
+      }
+
+      try {
+        frontendSetup.save(defaultConfig)
+        done('Did not throw!')
+      } catch (error) {
+        assert.equal(error.message, 'error')
+        done()
+      }
     })
   })
 })
