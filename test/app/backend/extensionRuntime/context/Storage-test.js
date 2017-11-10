bb16e6cf4a69ede6f5de8caec275637b69b59735
Metin Kul <metin.kul@shopgate.com>
2017-11-10 12:05:11 +0100

    refactored storage and context to mimic production environment better

--- test/app/backend/extensionRuntime/context/Storage-test.js
+++ test/app/backend/extensionRuntime/context/Storage-test.js
@@ -7,9 +7,11 @@ const fsExtra = require('fs-extra')
 
 describe('Storage', () => {
   let storagePath
+  let storage
   beforeEach(() => {
     storagePath = path.join('test', 'storage.json')
     process.env.STORAGE_PATH = storagePath
+    storage = new Storage()
   })
 
   afterEach(done => {
@@ -17,25 +19,146 @@ describe('Storage', () => {
     rimraf(storagePath, done)
   })
 
+  it('getInstance should give me the same instance', () => {
+    const storage = Storage.getInstance()
+    storage.foobar = 'barfoo'
+    assert.equal(Storage.getInstance(), storage)
+  })
+
   describe('constructor', () => {
     it('should construct with default path', () => {
       delete process.env.STORAGE_PATH
-      const storage = new Storage()
+      storage = new Storage()
       assert.equal(storage.path, path.join(AppSettings.SETTINGS_FOLDER, 'storage.json'))
+      assert.equal(storage.saveQueued, undefined)
+      assert.equal(storage.saving, undefined)
     })
 
     it('should construct empty', () => {
-      const storage = new Storage()
       assert.equal(storage.path, storagePath)
       assert.deepEqual(storage.data, {})
+      assert.equal(storage.saveQueued, undefined)
+      assert.equal(storage.saving, undefined)
     })
 
     it('should construct with data', () => {
       const data = {foo: 'bar', bar: 'foo'}
       fsExtra.writeJsonSync(storagePath, data)
-      const storage = new Storage()
+      storage = new Storage()
       assert.equal(storage.path, storagePath)
       assert.deepEqual(storage.data, data)
+      assert.equal(storage.saveQueued, undefined)
+      assert.equal(storage.saving, undefined)
+    })
+  })
+
+  describe('get', () => {
+    it('should cb undefined if path was not set', done => {
+      storage.get('foo/bar/foobar', (err, value) => {
+        assert.ifError(err)
+        assert.equal(value, undefined)
+        done()
+      })
+    })
+
+    it('should cb value if path was set set', done => {
+      const value = {foo: 'bar'}
+      const path = 'foo/bar/foobar'
+      storage.data[path] = value
+
+      storage.get(path, (err, v) => {
+        assert.ifError(err)
+        assert.equal(v, value)
+        done()
+      })
+    })
+  })
+
+  describe('set', () => {
+    it('should set value and call save', done => {
+      const value = {foo: 'bar'}
+      const path = 'foo/bar/foobar'
+      let doneCount = 0
+      function isDone () {
+        if (doneCount++ === 1) return done()
+      }
+
+      storage.save = isDone
+
+      storage.set(path, value, err => {
+        assert.ifError(err)
+        assert.equal(storage.data[path], value)
+        isDone()
+      })
+    })
+  })
+
+  describe('del', () => {
+    it('should delete value and call save', done => {
+      const path = 'foo/bar/foobar'
+      storage.data[path] = {foo: 'bar'}
+      let doneCount = 0
+      function isDone () {
+        if (doneCount++ === 1) return done()
+      }
+
+      storage.save = isDone
+
+      storage.del(path, err => {
+        assert.ifError(err)
+        assert.equal(storage.data[path], undefined)
+        isDone()
+      })
+    })
+  })
+
+  describe('save', () => {
+    it('should save and call saveCb', done => {
+      storage.data.foo = 'bar'
+      storage.data.bar = 'foo'
+
+      storage.saveCb = err => {
+        assert.ifError(err)
+        assert.equal(storage.saveQueued, undefined)
+        assert.equal(storage.saving, true)
+
+        fsExtra.readJson(storagePath, (err, data) => {
+          assert.ifError(err)
+          assert.deepEqual(data, storage.data)
+          done()
+        })
+      }
+      storage.save()
+    })
+
+    it('should set saveQueued to true if already saving', done => {
+      storage.saveCb = err => {
+        assert.ifError(err)
+        assert.equal(storage.saveQueued, undefined)
+        assert.equal(storage.saving, true)
+        storage.save()
+        assert.equal(storage.saveQueued, true)
+        assert.equal(storage.saving, true)
+        done()
+      }
+      storage.save()
+    })
+
+    it('should reset saving', () => {
+      storage.saving = true
+      storage.saveCb()
+      assert.equal(storage.saving, false)
+    })
+
+    it('should reset saving and call save again if saveQueued', done => {
+      storage.saving = true
+      storage.saveQueued = true
+      storage.save = () => {
+        assert.equal(storage.saving, false)
+        assert.equal(storage.saveQueued, false)
+        done()
+      }
+      storage.saveCb()
     })
   })
 })
