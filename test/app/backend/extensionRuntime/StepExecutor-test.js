ce8eeb6df26ce8e6d6aebf2ad540515bce115c94
Steffen Zull <steffen.zull@shopgate.com>
2017-12-11 14:17:47 +0100

    Fix tests

--- test/app/backend/extensionRuntime/StepExecutor-test.js
+++ test/app/backend/extensionRuntime/StepExecutor-test.js
@@ -174,10 +174,28 @@ describe('StepExecutor', () => {
       path: '@foo/bar/crashing2.js',
       meta: {appId: 'shop_123'}
     }
+
+    let exitCalled = false
+    executor.start = (cb) => {
+      assert.equal(executor.childProcess, undefined)
+      assert.ok(exitCalled)
+      done()
+    }
+
+    const onExit = executor.onExit.bind(executor)
+    executor.onExit = (code, signal) => {
+      if (!exitCalled) {
+        assert.equal(code, 1)
+        assert.equal(signal, null)
+      }
+      assert.ok(executor.childProcess)
+      exitCalled = true
+      onExit(code, signal)
+    }
+
     executor.execute(input, stepMeta, (err) => {
       assert.ok(err)
       assert.equal(err.message, 'Cannot read property \'notThere\' of undefined')
-      done()
     })
   })
 
