02724a24791917dabed4f079f362923e11decae8
Steffen Zull <steffen.zull@shopgate.com>
2018-01-04 15:25:54 +0100

    Refactor ExtensionAction

--- test/actions/ExtensionAction-test.js
+++ test/actions/ExtensionAction-test.js
@@ -13,181 +13,198 @@ const userSettingsFolder = path.join('build', 'usersettings')
 const appPath = path.join('build', 'appsettings')
 
 describe('ExtensionAction', () => {
-  const action = new ExtensionAction()
+  describe('attaching and detaching', () => {
+    const action = new ExtensionAction()
 
-  beforeEach(() => {
-    process.env.USER_PATH = userSettingsFolder
-    process.env.APP_PATH = appPath
-    const appId = 'foobarTest'
+    beforeEach(() => {
+      process.env.USER_PATH = userSettingsFolder
+      process.env.APP_PATH = appPath
+      const appId = 'foobarTest'
 
-    const appSettings = new AppSettings()
-    fsEx.emptyDirSync(path.join(appPath, AppSettings.SETTINGS_FOLDER))
-    appSettings.setId(appId).setAttachedExtensions({}).save().init()
-    AppSettings.setInstance(appSettings)
+      const appSettings = new AppSettings()
+      fsEx.emptyDirSync(path.join(appPath, AppSettings.SETTINGS_FOLDER))
+      appSettings.setId(appId).setAttachedExtensions({}).save().init()
+      AppSettings.setInstance(appSettings)
 
-    fsEx.emptyDirSync(userSettingsFolder)
-    UserSettings.getInstance().getSession().token = {}
-  })
-
-  afterEach((done) => {
-    UserSettings.setInstance()
-    AppSettings.setInstance()
-    delete process.env.USER_PATH
-    delete process.env.APP_PATH
-    async.parallel([
-      (cb) => fsEx.remove(userSettingsFolder, cb),
-      (cb) => fsEx.remove(appPath, cb)
-    ], done)
-  })
-
-  describe('general', () => {
-    it('should register', () => {
-      const commander = {}
-      commander.command = sinon.stub().returns(commander)
-      commander.description = sinon.stub().returns(commander)
-      commander.action = sinon.stub().returns(commander)
-      commander.option = sinon.stub().returns(commander)
-
-      action.register(commander)
-
-      assert(commander.command.calledWith('extension-create [types...]'))
-      assert(commander.description.calledWith('creates a new extension'))
-      assert(commander.option.calledWith('--extension [extensionName]', 'Name of the new extension'))
-      assert(commander.option.calledWith('--organization [organizationName]', 'Name of the organization this extension belongs to'))
-
-      assert(commander.command.calledWith('extension <action> [extensions...]'))
-      assert(commander.description.calledWith('attaches or detaches one or more extensions'))
-
-      assert(commander.action.callCount === 2)
+      fsEx.emptyDirSync(userSettingsFolder)
+      UserSettings.getInstance().getSession().token = {}
     })
 
-    it('should throw if user not logged in', () => {
-      UserSettings.getInstance().getSession().token = null
-      try {
-        action.run('attach')
-      } catch (err) {
-        assert.equal(err.message, 'not logged in')
-      }
+    afterEach((done) => {
+      UserSettings.setInstance()
+      AppSettings.setInstance()
+      delete process.env.USER_PATH
+      delete process.env.APP_PATH
+      async.parallel([
+        (cb) => fsEx.remove(userSettingsFolder, cb),
+        (cb) => fsEx.remove(appPath, cb)
+      ], done)
     })
 
-    it('should throw if invalid action is given', () => {
-      try {
-        action.run('invalid')
-      } catch (err) {
-        assert.equal(err.message, 'unknown action "invalid"')
-      }
-    })
-  })
+    describe('general', () => {
+      it('should register', () => {
+        const commander = {}
+        commander.command = sinon.stub().returns(commander)
+        commander.description = sinon.stub().returns(commander)
+        commander.action = sinon.stub().returns(commander)
+        commander.option = sinon.stub().returns(commander)
 
-  describe('attaching', () => {
-    it('should attach if extension exists locally', () => {
-      const name = 'existentExtension'
-      const id = 'extId'
+        action.register(commander)
 
-      const extPath = path.join(appPath, 'extensions', name)
-      fsEx.ensureDirSync(extPath)
-      fsEx.writeJSONSync(path.join(extPath, 'extension-config.json'), {id})
+        assert(commander.command.calledWith('extension-create [types...]'))
+        assert(commander.description.calledWith('creates a new extension'))
+        assert(commander.option.calledWith('--extension [extensionName]', 'Name of the new extension'))
+        assert(commander.option.calledWith('--organization [organizationName]', 'Name of the organization this extension belongs to'))
+        assert(commander.option.calledWith('--trusted [type]', 'only valid if you\'re about to create a backend extension'))
 
-      action.run('attach', [name])
-      assert.deepEqual(AppSettings.getInstance().attachedExtensions[id], {path: name, trusted: false})
-    })
+        assert(commander.command.calledWith('extension <action> [extensions...]'))
+        assert(commander.description.calledWith('attaches or detaches one or more extensions'))
 
-    it('should attach all local extensions if no one is given', () => {
-      const name1 = 'existentExtension1'
-      const name2 = 'existentExtension2'
+        assert(commander.action.callCount === 2)
+      })
 
-      const extPath1 = path.join(appPath, 'extensions', name1)
-      const extPath2 = path.join(appPath, 'extensions', name2)
-      fsEx.ensureDirSync(extPath1)
-      fsEx.ensureDirSync(extPath2)
-      fsEx.writeJSONSync(path.join(extPath1, 'extension-config.json'), {id: 'existentExtension1'})
-      fsEx.writeJSONSync(path.join(extPath2, 'extension-config.json'), {id: 'existentExtension2'})
+      it('should throw if user not logged in', () => {
+        UserSettings.getInstance().getSession().token = null
+        try {
+          action.run('attach')
+        } catch (err) {
+          assert.equal(err.message, 'not logged in')
+        }
+      })
 
-      action.run('attach', [name1])
-      action.run('attach')
-
-      assert.deepEqual(AppSettings.getInstance().attachedExtensions, {
-        existentExtension1: { path: 'existentExtension1', trusted: false },
-        existentExtension2: { path: 'existentExtension2', trusted: false }
+      it('should throw if invalid action is given', () => {
+        try {
+          action.run('invalid')
+        } catch (err) {
+          assert.equal(err.message, 'unknown action "invalid"')
+        }
       })
     })
 
-    it('should throw an error if extension does not exist locally', () => {
-      const name = 'notExitstentExtension'
+    describe('attaching', () => {
+      it('should attach if extension exists locally', () => {
+        const name = 'existentExtension'
+        const id = 'extId'
 
-      try {
+        const extPath = path.join(appPath, 'extensions', name)
+        fsEx.ensureDirSync(extPath)
+        fsEx.writeJSONSync(path.join(extPath, 'extension-config.json'), {id})
+
         action.run('attach', [name])
-      } catch (e) {
-        assert.equal(e.message, `Config file of 'notExitstentExtension' is invalid or not existent`)
-      }
+        assert.deepEqual(AppSettings.getInstance().attachedExtensions[id], {path: name, trusted: false})
+      })
+
+      it('should attach all local extensions if no one is given', () => {
+        const name1 = 'existentExtension1'
+        const name2 = 'existentExtension2'
+
+        const extPath1 = path.join(appPath, 'extensions', name1)
+        const extPath2 = path.join(appPath, 'extensions', name2)
+        fsEx.ensureDirSync(extPath1)
+        fsEx.ensureDirSync(extPath2)
+        fsEx.writeJSONSync(path.join(extPath1, 'extension-config.json'), {id: 'existentExtension1'})
+        fsEx.writeJSONSync(path.join(extPath2, 'extension-config.json'), {id: 'existentExtension2'})
+
+        action.run('attach', [name1])
+        action.run('attach')
+
+        assert.deepEqual(AppSettings.getInstance().attachedExtensions, {
+          existentExtension1: { path: 'existentExtension1', trusted: false },
+          existentExtension2: { path: 'existentExtension2', trusted: false }
+        })
+      })
+
+      it('should throw an error if extension does not exist locally', () => {
+        const name = 'notExitstentExtension'
+
+        try {
+          action.run('attach', [name])
+        } catch (e) {
+          assert.equal(e.message, `Config file of 'notExitstentExtension' is invalid or not existent`)
+        }
+      })
+
+      it('should throw an error if extension-config is invalid', () => {
+        const name = 'existentExtension'
+
+        const extPath = path.join(appPath, 'extensions', name)
+        fsEx.ensureDirSync(extPath)
+        fsEx.writeJSONSync(path.join(extPath, 'extension-config.json'), {})
+
+        try {
+          action.run('attach', [name])
+        } catch (e) {
+          assert.equal(e.message, `Config file of '${name}' is invalid or not existent`)
+        }
+
+        assert.deepEqual(AppSettings.getInstance().attachedExtensions, {})
+      })
+
+      it('should throw an error if extension is already attached', () => {
+        const name = 'existentExtension'
+
+        const extPath = path.join(appPath, 'extensions', name)
+        fsEx.ensureDirSync(extPath)
+        fsEx.writeJSONSync(path.join(extPath, 'extension-config.json'), {id: name})
+
+        action.run('attach', [name])
+        try {
+          action.run('attach', [name])
+        } catch (e) {
+          assert.equal(e.message, `Extension 'existentExtension (existentExtension) is already attached`)
+        }
+      })
     })
 
-    it('should throw an error if extension-config is invalid', () => {
-      const name = 'existentExtension'
+    describe('detaching', () => {
+      it('should detach a extension', () => {
+        const name = 'existentExtension'
 
-      const extPath = path.join(appPath, 'extensions', name)
-      fsEx.ensureDirSync(extPath)
-      fsEx.writeJSONSync(path.join(extPath, 'extension-config.json'), {})
+        const extPath = path.join(appPath, 'extensions', name)
+        fsEx.ensureDirSync(extPath)
+        fsEx.writeJSONSync(path.join(extPath, 'extension-config.json'), {id: name})
 
-      try {
-        action.run('attach', [name])
-      } catch (e) {
-        assert.equal(e.message, `Config file of '${name}' is invalid or not existent`)
-      }
+        AppSettings.getInstance().attachExtension(name, {id: name, trusted: false})
+        action.run('detach', [name])
+        assert.deepEqual(AppSettings.getInstance().attachedExtensions, {})
+      })
 
-      assert.deepEqual(AppSettings.getInstance().attachedExtensions, {})
-    })
+      it('should skip if extension was not attached', (done) => {
+        const name = 'notExitstentExtension'
 
-    it('should throw an error if extension is already attached', () => {
-      const name = 'existentExtension'
+        const extPath = path.join(appPath, 'extensions', name)
+        fsEx.ensureDirSync(extPath)
+        fsEx.writeJSONSync(path.join(extPath, 'extension-config.json'), {id: name})
 
-      const extPath = path.join(appPath, 'extensions', name)
-      fsEx.ensureDirSync(extPath)
-      fsEx.writeJSONSync(path.join(extPath, 'extension-config.json'), {id: name})
+        logger.warn = (text) => {
+          assert.equal(text, `The extension '${name}' is not attached`)
+          done()
+        }
 
-      action.run('attach', [name])
-      try {
-        action.run('attach', [name])
-      } catch (e) {
-        assert.equal(e.message, `Extension 'existentExtension (existentExtension) is already attached`)
-      }
+        action.run('detach', [name])
+      })
+
+      it('should detach all extensions if none was specified', () => {
+        AppSettings.getInstance().attachExtension('ext1', {id: 'ext1'})
+        AppSettings.getInstance().attachExtension('ext2', {id: 'ext2'})
+
+        action.run('detach')
+        assert.deepEqual(AppSettings.getInstance().attachedExtensions, {})
+      })
     })
   })
 
-  describe('detaching', () => {
-    it('should detach a extension', () => {
-      const name = 'existentExtension'
+  // describe('extension create', () => {
+  //   describe('general', () => {
+  //     it('should create an extension', (done) => {
+  //       const action = new ExtensionAction()
 
-      const extPath = path.join(appPath, 'extensions', name)
-      fsEx.ensureDirSync(extPath)
-      fsEx.writeJSONSync(path.join(extPath, 'extension-config.json'), {id: name})
+  //       done()
+  //     })
 
-      AppSettings.getInstance().attachExtension(name, {id: name, trusted: false})
-      action.run('detach', [name])
-      assert.deepEqual(AppSettings.getInstance().attachedExtensions, {})
-    })
-
-    it('should skip if extension was not attached', (done) => {
-      const name = 'notExitstentExtension'
-
-      const extPath = path.join(appPath, 'extensions', name)
-      fsEx.ensureDirSync(extPath)
-      fsEx.writeJSONSync(path.join(extPath, 'extension-config.json'), {id: name})
-
-      logger.warn = (text) => {
-        assert.equal(text, `The extension '${name}' is not attached`)
-        done()
-      }
-
-      action.run('detach', [name])
-    })
-
-    it('should detach all extensions if none was specified', () => {
-      AppSettings.getInstance().attachExtension('ext1', {id: 'ext1'})
-      AppSettings.getInstance().attachExtension('ext2', {id: 'ext2'})
-
-      action.run('detach')
-      assert.deepEqual(AppSettings.getInstance().attachedExtensions, {})
-    })
-  })
+  //     it('should fail because of sth.', (done) => {
+  //       done()
+  //     })
+  //   })
+  // })
 })
