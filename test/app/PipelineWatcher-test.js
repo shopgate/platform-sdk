44454027e8f6f694cfd6ad281d8ddb57bf4797dd
Timo Ebel <timo.ebel@shopgate.com>
2018-01-16 15:39:49 +0100

    CLOUDSDK-126 Fixed unit tests for PipelineWatcher.

--- test/app/PipelineWatcher-test.js
+++ test/app/PipelineWatcher-test.js
@@ -3,27 +3,30 @@ const path = require('path')
 const fsEx = require('fs-extra')
 const PipelineWatcher = require('../../lib/app/backend/PipelineWatcher')
 const utils = require('../../lib/utils/utils')
+const AppSettings = require('../../lib/app/AppSettings')
 
 const appPath = path.join('build', 'appsettings')
 
 describe('PipelineWatcher', () => {
   let pipelineWatcher
+  let extensionsPipelineFolder
 
-  beforeEach((done) => {
+  beforeEach(async () => {
     process.env.APP_PATH = appPath
+    extensionsPipelineFolder = path.join(utils.getApplicationFolder(), AppSettings.EXTENSIONS_FOLDER, 'testExtension', 'pipelines')
     pipelineWatcher = new PipelineWatcher({getApplicationFolder: utils.getApplicationFolder})
-    fsEx.emptyDirSync(path.join(pipelineWatcher.appFolder, 'pipelines'))
-    pipelineWatcher.start(done)
+    fsEx.emptyDirSync(extensionsPipelineFolder)
+    await pipelineWatcher.start()
   })
 
   afterEach((done) => {
     delete process.env.APP_PATH
     pipelineWatcher.close()
-    fsEx.remove(appPath, done)
+    fsEx.remove(extensionsPipelineFolder, done)
   })
 
   it('should emit changed pipeline', (done) => {
-    const pipelinePath = path.join(pipelineWatcher.appFolder, 'pipelines', 'somePipeline.json')
+    const pipelinePath = path.join(extensionsPipelineFolder, 'somePipeline.json')
 
     pipelineWatcher.on('all', (event, file) => {
       assert.equal(event, 'add')
@@ -31,7 +34,7 @@ describe('PipelineWatcher', () => {
       done()
     })
 
-    fsEx.writeJson(pipelinePath, {someAttribtue: '2'}, () => {})
+    fsEx.writeJson(pipelinePath, {someAttribtue: '2'}, (err) => { assert.ifError(err) })
   })
 
   it('should stop watching', () => {
