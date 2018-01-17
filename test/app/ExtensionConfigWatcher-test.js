e360fc46ae689c859c66817f23b9d3fe5e7b6c61
Timo Ebel <timo.ebel@shopgate.com>
2018-01-17 12:03:12 +0100

    CLOUDSDK-126 Fixed afterEach() not waiting for fs-extra.remove() to be finished could cause race condition when multiple tests try writing to same files.

--- test/app/ExtensionConfigWatcher-test.js
+++ test/app/ExtensionConfigWatcher-test.js
@@ -20,7 +20,7 @@ describe('ExtensionConfigWatcher', () => {
     delete process.env.APP_PATH
 
     await extensionConfigWatcher.stop()
-    fsEx.remove(appPath)
+    await fsEx.remove(appPath)
   })
 
   it('should emit changed config', (done) => {
