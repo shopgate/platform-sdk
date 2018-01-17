064d4b48610b6ef7967fb97c49f7431bd6be1dcf
Steffen Zull <steffen.zull@shopgate.com>
2018-01-17 12:21:50 +0100

    Refactor BackendProcess tests

--- test/app/backend/BackendProcess-test.js
+++ test/app/backend/BackendProcess-test.js
@@ -45,7 +45,8 @@ describe('BackendProcess', () => {
       start: () => sinon.stub().resolves(),
       stop: () => sinon.stub().resolves(),
       startWatcher: () => sinon.stub().resolves(),
-      stopWatcher: () => sinon.stub().resolves()
+      stopWatcher: () => sinon.stub().resolves(),
+      execute: (input, stepMetaData, cb) => cb(null, null)
     }
 
     portfinder.getPort((err, port) => {
@@ -73,6 +74,103 @@ describe('BackendProcess', () => {
     ])
   })
 
+  describe('connect', () => {
+    it('should react to all events', (done) => {
+      let warnWasCalled = false
+      let errorWasCalled = false
+      let stepCallWasCalled = false
+      let updateTokenWasCalled = false
+
+      logger.warn = (message) => {
+        assert.equal(message, 'Connection error! Trying to reconnect...')
+        warnWasCalled = true
+      }
+
+      logger.error = (err) => {
+        assert.equal(err.message, 'error')
+        errorWasCalled = true
+      }
+
+      backendProcess.stepCall = (data, cb) => {
+        assert.deepEqual(data, {foo: 'bar'})
+        stepCallWasCalled = true
+      }
+
+      backendProcess.updateToken = (data) => {
+        assert.deepEqual(data, {foo: 'bar'})
+        updateTokenWasCalled = true
+      }
+
+      backendProcess.connect().then(() => {
+        socketIOMock.emit('connect_error')
+        socketIOMock.emit('error', new Error('error'))
+        socketIOMock.emit('stepCall', {foo: 'bar'})
+        socketIOMock.emit('updateToken', {foo: 'bar'})
+        assert.ok(warnWasCalled)
+        assert.ok(errorWasCalled)
+        assert.ok(stepCallWasCalled)
+        assert.ok(updateTokenWasCalled)
+        done()
+      })
+    })
+  })
+
+  describe('attachExtension', () => {
+    it('should forward on extensions attach', () => {
+      let wasCalled = 0
+      socketIOMock.on('registerExtension', (data, cb) => {
+        assert.deepEqual(data, {extensionId: 'testExt', trusted: false})
+        cb()
+        wasCalled++
+      })
+
+      return backendProcess.connect()
+        .then(() => backendProcess.attachExtension({id: 'testExt', trusted: false}))
+        .then(() => {
+          assert.equal(wasCalled, 1)
+        })
+    })
+
+    it('should log an error', (done) => {
+      logger.error = (message) => {
+        assert.ok(message.startsWith('Error while attaching the extension'))
+        done()
+      }
+
+      backendProcess._emitToSocket = () => new Promise((resolve, reject) => reject(new Error('error')))
+
+      backendProcess.attachExtension({id: 'null', trusted: false})
+    })
+  })
+
+  describe('detachExtension', () => {
+    it('should forward on extensions detach', () => {
+      let wasCalled = 0
+      socketIOMock.on('deregisterExtension', (data, cb) => {
+        assert.deepEqual(data, {extensionId: 'testExt', trusted: false})
+        cb()
+        wasCalled++
+      })
+
+      return backendProcess.connect()
+        .then(() => backendProcess.detachExtension({id: 'testExt', trusted: false}))
+        .then(() => {
+          assert.equal(wasCalled, 1)
+        })
+    })
+
+    it('should log an error', (done) => {
+      logger.error = (message) => {
+        assert.ok(message.startsWith('Error while detaching the extension'))
+        done()
+      }
+
+      backendProcess._emitToSocket = () => new Promise((resolve, reject) => reject(new Error('error')))
+
+      backendProcess.detachExtension({id: 'null', trusted: false})
+    })
+  })
+
   describe('select application', () => {
     it('should select an application', async () => {
       let wasCalled = 0
@@ -88,110 +186,97 @@ describe('BackendProcess', () => {
       assert.equal(wasCalled, 1)
     })
 
-    it('should fail if socket sends error', async () => {
+    it('should fail if socket sends error', (done) => {
       socketIOMock.on('selectApplication', (data, cb) => {
         const err = new Error('Forbidden')
         err.code = 403
         cb(err)
       })
 
-      try {
-        await backendProcess.connect()
-        await backendProcess.selectApplication('123')
-      } catch (err) {
-        assert.ok(err)
-        assert.equal(err.code, 403)
-        assert.equal(err.message, 'Forbidden')
+      backendProcess.connect()
+        .then(() => backendProcess.selectApplication('123'))
+        .catch(err => {
+          assert.ok(err)
+          assert.equal(err.code, 403)
+          assert.equal(err.message, 'Forbidden')
+          done()
+        })
+    })
+  })
+
+  describe('resetPipelines', () => {
+    it('should log `Reset application`', (done) => {
+      logger.info = (message) => {
+        if (message === 'Establishing SDK connection') return
+
+        assert.equal(message, 'Reset application')
+        done()
       }
+
+      socketIOMock.on('reset', (data, cb) => cb(null))
+
+      backendProcess.connect()
+      .then(() => backendProcess.resetPipelines())
     })
 
-    it('should fail if socket sends error (reset)', async () => {
-      socketIOMock.on('selectApplication', (data, cb) => {
-        assert.deepEqual(data, {applicationId: 'shop_10006'})
-        cb()
-      })
-
+    it('should fail if socket sends error (resetPipelines)', (done) => {
       socketIOMock.on('reset', (data, cb) => cb(new Error('error')))
 
-      try {
-        await backendProcess.connect()
-        await backendProcess.selectApplication('shop_10006')
-      } catch (err) {
-        assert.ok(err)
-        assert.equal(err.message, 'error')
-      }
-    })
-
-    it('should fail if socket sends error (reload)', async () => {
-      socketIOMock.on('selectApplication', (data, cb) => {
-        assert.deepEqual(data, {applicationId: 'shop_10006'})
-        cb()
-      })
-
-      socketIOMock.on('reset', (data, cb) => cb())
-      socketIOMock.on('reload', (data, cb) => cb(new Error('error')))
-
-      try {
-        await backendProcess.connect()
-        await backendProcess.selectApplication('shop_10006')
-      } catch (err) {
-        assert.ok(err)
-        assert.equal(err.message, 'error')
-      }
-    })
-
-    it('should forward on extensions attach', async () => {
-      let wasCalled = 0
-      socketIOMock.on('registerExtension', (data, cb) => {
-        assert.deepEqual(data, {extensionId: 'testExt', trusted: false})
-        cb()
-        wasCalled++
-      })
-
-      await backendProcess.connect()
-      await backendProcess.attachExtension({id: 'testExt', trusted: false})
-
-      assert.equal(wasCalled, 1)
-    })
-
-    it('should forward on extensions detach', async () => {
-      let wasCalled = 0
-      socketIOMock.on('deregisterExtension', (data, cb) => {
-        assert.deepEqual(data, {extensionId: 'testExt', trusted: false})
-        cb()
-        wasCalled++
-      })
-
-      await backendProcess.connect()
-      await backendProcess.detachExtension({id: 'testExt', trusted: false})
-
-      assert.equal(wasCalled, 1)
+      backendProcess.connect()
+        .then(() => backendProcess.resetPipelines())
+        .catch(err => {
+          assert.ok(err)
+          assert.equal(err.message, 'error')
+          done()
+        })
     })
   })
 
-  describe('attachExtension', () => {
-    it('should log an error', (done) => {
-      logger.error = (message) => {
-        assert.ok(message.startsWith('Error while attaching the extension'))
+  describe('reload', () => {
+    it('should log `Activated local pipelines on the remote server`', (done) => {
+      logger.info = (message) => {
+        if (message === 'Establishing SDK connection') return
+
+        assert.equal(message, 'Activated local pipelines on the remote server')
         done()
       }
 
-      backendProcess._emitToSocket = () => new Promise((resolve, reject) => reject(new Error('error')))
+      socketIOMock.on('reloadPipelines', (data, cb) => cb(null))
 
-      backendProcess.attachExtension({id: 'null', trusted: false})
+      backendProcess.connect()
+      .then(() => backendProcess.reloadPipelineController())
+    })
+
+    it('should fail if socket sends error (reload)', (done) => {
+      socketIOMock.on('reloadPipelines', (data, cb) => cb(new Error('error')))
+
+      backendProcess.connect()
+        .then(() => backendProcess.reloadPipelineController())
+        .catch(err => {
+          assert.ok(err)
+          assert.equal(err.message, 'error')
+          done()
+        })
     })
   })
 
-  describe('detachExtension', () => {
-    it('should log an error', (done) => {
-      logger.error = (message) => {
-        assert.ok(message.startsWith('Error while detaching the extension'))
+  describe('startStepExecutor', () => {
+    it('should start the step executor', () => {
+      return backendProcess.startStepExecutor()
+    })
+  })
+
+  describe('stepCall', () => {
+    it('should call a step', (done) => {
+      stepExecutor.execute = (input, stepMetaData, cb) => cb(null, {input, stepMetaData})
+
+      const data = { input: 'i', stepMetaData: 's' }
+
+      backendProcess.stepCall(data, (err, result) => {
+        assert.ifError(err)
+        assert.deepEqual(data, result)
         done()
-      }
-
-      backendProcess._emitToSocket = () => new Promise((resolve, reject) => reject(new Error('error')))
-
-      backendProcess.detachExtension({id: 'null', trusted: false})
+      })
     })
   })
 
@@ -201,6 +286,19 @@ describe('BackendProcess', () => {
     it('should update the token', () => {
       backendProcess.updateToken(token)
       assert.deepEqual(userSettings.getToken(), token)
+    })
+  })
+
+  describe('disconnect', (done) => {
+    it('should fail because executor.stop() fails', () => {
+      logger.debug = (message) => {
+        assert.equal(message, 'Error: error')
+      }
+
+      stepExecutor.stop = () => new Promise((resolve, reject) => reject(new Error('error')))
+
+      return backendProcess.connect()
+        .then(() => backendProcess.disconnect())
     })
   })
 })
