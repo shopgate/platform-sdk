3b96705aad84c0c90b415bc06542409ddc8df756
Timo Ebel <timo.ebel@shopgate.com>
2018-01-26 15:35:52 +0100

    CLOUDSDK-150 Fixed unit tests.

--- test/actions/InitAction-test.js
+++ test/actions/InitAction-test.js
@@ -46,12 +46,11 @@ describe('InitAction', () => {
   it('should throw if user not logged in', (done) => {
     userSettings.setToken(null)
     const init = new InitAction()
-    try {
-      init.run(null)
-    } catch (e) {
-      assert.equal(e.message, 'You\'re not logged in! Please run `sgcloud login` again.')
-      done()
-    }
+    init.run(null)
+      .catch(err => {
+        assert.equal(err.message, 'You\'re not logged in! Please run `sgcloud login` again.')
+        done()
+      })
   })
 
   it('should reinit the application if selected', (done) => {
