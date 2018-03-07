0ce5e731849dc7d60844e596c5a5785cd04b3115
Timo Ebel <timo.ebel@shopgate.com>
2018-03-07 14:39:45 +0100

    CLOUDSDK-180 Fixed/adapted unit tests for storage operations.

--- test/app/backend/extensionRuntime/context/Storage-test.js
+++ test/app/backend/extensionRuntime/context/Storage-test.js
@@ -29,75 +29,103 @@ describe('Storage', () => {
   })
 
   describe('get', () => {
-    it('should cb undefined if path was not set', done => {
-      fsEx.readJSON = (file, options, cb) => { cb(new Error('file not found')) }
+    it('should cb undefined if path was not set', () => {
+      fsEx.readJSON = () => { throw new Error('file not found') }
 
       const storage = new Storage(appSettings, log)
-      storage.get('foo/bar/foobar', (err, value) => {
-        assert.ifError(err)
-        assert.equal(value, undefined)
-        done()
+
+      return new Promise((resolve, reject) => {
+        storage.get('foo/bar/foobar', (err, actual) => {
+          try {
+            assert.ifError(err)
+            assert.equal(actual, undefined)
+          } catch (err) {
+            reject(err)
+          }
+          resolve()
+        })
       })
     })
 
-    it('should cb value if path was set', done => {
-      const value = {foo: 'bar'}
+    it('should cb value if path was set', () => {
+      const expected = { foo: 'bar' }
       const path = 'foo/bar/foobar'
-      fsEx.readJSON = (file, options, cb) => {
-        cb(null, {
-          'foo/bar/foobar': value
-        })
+      fsEx.readJSON = () => {
+        return {
+          'foo/bar/foobar': expected
+        }
       }
 
       const storage = new Storage(appSettings, log)
-      storage.get(path, (err, v) => {
-        assert.ifError(err)
-        assert.equal(v, value)
-        done()
+
+      return new Promise((resolve, reject) => {
+        storage.get(path, (err, actual) => {
+          try {
+            assert.ifError(err)
+            assert.equal(actual, expected)
+          } catch (err) {
+            reject(err)
+          }
+          resolve()
+        })
       })
     })
   })
 
   describe('set', () => {
-    it('should set value and call save', done => {
-      const value = {foo: 'bar'}
+    it('should set value and call save', () => {
+      const value = { foo: 'bar' }
       const path = 'foo/bar/foobar'
       let called = 0
 
-      fsEx.readJSON = (file, options, cb) => cb(null, {})
-      fsEx.writeJSON = (file, content, options, cb) => {
+      fsEx.readJSON = () => { return {} }
+      fsEx.writeJSON = (file, content) => {
         assert.equal(content[path], value)
         called++
-        cb(null)
       }
 
       const storage = new Storage(appSettings, log)
-      storage.set(path, value, err => {
-        assert.ifError(err)
-        assert.equal(called, 1)
-        done()
+
+      return new Promise((resolve, reject) => {
+        storage.set(path, value, err => {
+          try {
+            assert.ifError(err)
+            assert.equal(called, 1)
+          } catch (err) {
+            reject(err)
+          }
+
+          resolve()
+        })
       })
     })
   })
 
   describe('del', () => {
-    it('should delete value and call save', done => {
-      const value = {foo: 'bar'}
+    it('should delete value and call save', () => {
+      const value = { foo: 'bar' }
       const path = 'foo/bar/foobar'
       let called = 0
 
-      fsEx.readJSON = (file, options, cb) => cb(null, {'foo/bar/foobar': value})
-      fsEx.writeJSON = (file, content, options, cb) => {
+      fsEx.readJSON = () => { return { 'foo/bar/foobar': value } }
+      fsEx.writeJSON = (file, content) => {
         assert.equal(content[path], undefined)
         called++
-        cb(null)
       }
 
       const storage = new Storage(appSettings, log)
-      storage.del(path, err => {
-        assert.ifError(err)
-        assert.equal(called, 1)
-        done()
+
+      return new Promise((resolve, reject) => {
+        storage.del(path, err => {
+          try {
+            assert.ifError(err)
+            assert.equal(called, 1)
+          } catch (err) {
+            reject(err)
+          }
+
+          resolve()
+        })
       })
     })
   })
