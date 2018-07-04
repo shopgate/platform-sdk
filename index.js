8c073279d6b5477f61e78fc28ad80fac1f9d8e96
Steffen Zull <steffen.zull@shopgate.com>
2018-07-04 18:35:25 +0200

    Update error handling when reloading pipeline controller fails

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const path = require('path')
 const glob = require('glob')
 const logger = require('./lib/logger')
-const actionFiles = glob.sync('./lib/actions/*.js', {cwd: __dirname, strict: true})
+const actionFiles = glob.sync('./lib/actions/*.js', { cwd: __dirname, strict: true })
 
 actionFiles.forEach((actionFile) => {
   module.exports[path.basename(actionFile).split('.')[0].split('Action')[0]] = require(actionFile)
